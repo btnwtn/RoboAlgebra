7df7e18fddb0c3e95007aeca49543c374bb60052
Brandon Newton <btnwtn@gmail.com>
2017-04-14 15:20:34 -0700

    Apply prettier to files

--- src/App.js
+++ src/App.js
@@ -1,139 +1,150 @@
-import React, { Component } from 'react';
-import { createStore } from 'redux'
-import { connect, Provider } from 'react-redux'
-import { DragDropContext, DragSource, DropTarget } from 'react-dnd'
-import HTML5Backend from 'react-dnd-html5-backend'
-import './App.css';
+import React, { Component } from "react";
+import { createStore } from "redux";
+import { connect, Provider } from "react-redux";
+import { DragDropContext, DragSource, DropTarget } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+import "./App.css";
 
 const initialState = {
   output: []
-}
+};
 
-let store = createStore(function (state = initialState, action) {
-  switch (action.type) {
-    case 'DISPLAY_OUTPUT':
-      return {
-        output: [
-          ...state.output,
-          ...action.payload.output
-        ]
-      }
-    default: return state
-  }
-}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+let store = createStore(
+  function(state = initialState, action) {
+    switch (action.type) {
+      case "DISPLAY_OUTPUT":
+        return {
+          output: [...state.output, ...action.payload.output]
+        };
+      default:
+        return state;
+    }
+  },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const Types = {
-  EMOJI: 'EMOJI',
-}
+  EMOJI: "EMOJI"
+};
 
 const emojiSource = {
   beginDrag(props) {
     return {
       children: props.children
-    }
+    };
   }
-}
+};
 
 function collect(connect, monitor) {
   return {
     connectDragSource: connect.dragSource(),
     isDragging: monitor.isDragging()
-  }
+  };
 }
 
 const Emoji = DragSource(Types.EMOJI, emojiSource, collect)(
   class Emoji extends Component {
     render() {
-      const { connectDragSource, isDragging } = this.props
+      const { connectDragSource, isDragging } = this.props;
       return connectDragSource(
-        <span style={{
-          opacity: isDragging ? 0.5 : 1,
-          border: '2px solid silver',
-          fontSize: '2rem',
-          marginRight: '1rem',
-          zIndex: 2,
-          cursor: 'move'
-        }}>
+        <span
+          style={{
+            opacity: isDragging ? 0.5 : 1,
+            border: "2px solid silver",
+            fontSize: "2rem",
+            marginRight: "1rem",
+            zIndex: 2,
+            cursor: "move"
+          }}
+        >
           {this.props.children}
         </span>
-      )
+      );
     }
   }
-)
+);
 
 const squareTarget = {
   drop(props, monitor) {
     store.dispatch({
-      type: 'DISPLAY_OUTPUT',
+      type: "DISPLAY_OUTPUT",
       payload: {
-        output: [`${monitor.getItem().children.repeat(3)}🐢`] 
+        output: [`${monitor.getItem().children.repeat(3)}🐢`]
       }
-    })
+    });
   }
-}
+};
 
 function squareCollect(connect, monitor) {
   return {
     connectDropTarget: connect.dropTarget(),
     isOver: monitor.isOver()
-  }
+  };
 }
 
 const Question = `function (emoji) {
 
   return emoji * 3 + 🐢
 
-}`
+}`;
 
 const Square = DropTarget(Types.EMOJI, squareTarget, squareCollect)(
   class Square extends Component {
     render() {
-      const { connectDropTarget, isOver } = this.props
+      const { connectDropTarget, isOver } = this.props;
       return connectDropTarget(
-        <pre style={{
-          padding: '1rem',
-          fontSize: '1.5rem',
-          backgroundColor: isOver ? 'yellow' : 'silver'
-        }}>
+        <pre
+          style={{
+            maxWidth: "26.25rem",
+            padding: "1rem",
+            fontSize: "1.5rem",
+            backgroundColor: isOver ? "yellow" : "#eee",
+            border: "2px solid #dedede"
+          }}
+        >
           {Question}
         </pre>
-      )
+      );
     }
   }
-)
+);
 
 function mapStateToProps({ output }) {
   return {
     output
-  }
+  };
 }
 
-const Output = connect(mapStateToProps)(
-  (props) => {
-    return (
-      <div>
-        <h2>Results:</h2>
+const Output = connect(mapStateToProps)(props => {
+  return (
+    <div>
+      <h2>Results:</h2>
       {props.output.map((emoji, i) => (
         <div
           key={`emoji-${i}`}
           style={{
-            fontSize: '2rem',
+            fontSize: "2rem"
           }}
         >
           {emoji}
         </div>
       ))}
-      </div>
-    )
-  }
-)
+    </div>
+  );
+});
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div>
-          <h1>RoboAlgebra</h1>
+          <h1
+            style={{
+              marginTop: 0
+            }}
+          >
+            RoboAlgebra
+          </h1>
           <Emoji>
             ☺️
           </Emoji>
@@ -144,8 +155,8 @@ class App extends Component {
             👽👽
           </Emoji>
           <h2>Drag an Emoji into the code below:</h2>
-          <Square/>
-          <Output/>
+          <Square />
+          <Output />
         </div>
       </Provider>
     );
