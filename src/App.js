5edfbbef584d1b6e87781c2eefdb7d095a4d0276
Brandon Newton <btnwtn@gmail.com>
2017-04-15 18:18:20 -0700

    THE TUBE

--- src/App.js
+++ src/App.js
@@ -66,6 +66,55 @@ const Output = connect(mapStateToProps)(props => {
   );
 });
 
+class Tube extends Component {
+  render() {
+    return (
+      <div style={{
+        position: 'relative',
+        marginTop: '10px',
+        marginBottom: '10px',
+      }}>
+        <div style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100px',
+          height: '20px',
+          backgroundColor: 'darkgreen',
+          borderRadius: '50%',
+          transform: 'translateY(-10px)',
+        }}/>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100px',
+          height: '200px',
+          backgroundColor: 'green',
+        }}>
+          <span style={{
+            fontSize: '1.4rem',
+            color: '#fff',
+            textShadow: '1px 1px 2px rgba(0,0,0,.4)',
+          }}>
+            2x + 3
+          </span>
+        </div>
+        <div style={{
+          position: 'absolute',
+          bottom: '-20px',
+          left: 0,
+          width: '100px',
+          height: '20px',
+          backgroundColor: 'green',
+          borderRadius: '50%',
+          transform: 'translateY(-10px)',
+        }}/>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   state = {
     question: 1
@@ -137,6 +186,9 @@ class App extends Component {
           </div>
 
           <h2>Drag an Emoji into the code below:</h2>
+
+          <Tube/>
+
           {this.state.question === 1 &&
             <FunctionBlock
               handleDrop={monitor => {
